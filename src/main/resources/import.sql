afb0f0c25d17061cdd475629a094c41d10ad4cd7
vajpai.bingi <vajpai.bingi@vajpai>
2016-12-03 22:58:18 +0530

    boolean value not accepting null.Updated the script.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO t_user (USER_ID, USERNAME, PASSWORD, FIRSTNAME, LASTNAME, EMAIL, IS_ACTIVE, created_date) VALUES (1, 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 'admin', 'admin@admin.com', 1, now());
+INSERT INTO t_user (USER_ID, USERNAME, PASSWORD, FIRSTNAME, LASTNAME, EMAIL, IS_ACTIVE, created_date,is_matrimony_user) VALUES (1, 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 'admin', 'admin@admin.com', 1, now(),1);
 
 INSERT INTO t_role (ROLE_ID, role_name) VALUES (1, 'ROLE_USER');
 INSERT INTO t_role (ROLE_ID, role_name) VALUES (2, 'ROLE_ADMIN');
