d3cc65a842d1a3ed89437b0d68921c0fa40a1735
Renukeswar <crenukeswar2010@gmail.com>
2016-12-22 12:35:13 +0530

    helping hand repo

--- src/main/java/com/devangam/repository/EducationHelpingHandRepository.java
+++ src/main/java/com/devangam/repository/EducationHelpingHandRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devangam.entity.Education;
 
+
 public interface EducationHelpingHandRepository extends JpaRepository<Education, Serializable>{
 
 }
