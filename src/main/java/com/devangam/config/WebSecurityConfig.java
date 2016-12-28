1e706a66c537f1f7068d87cf27664e873a073c44
vajpai.bingi <vajpai.bingi@vajpai>
2016-12-28 23:26:05 +0530

    code cleanup changes added

--- src/main/java/com/devangam/config/WebSecurityConfig.java
+++ src/main/java/com/devangam/config/WebSecurityConfig.java
@@ -70,15 +70,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 // we don't need CSRF because our token is invulnerable
                 .csrf().disable()
-
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-
                 // don't create session
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-
                 .authorizeRequests()
                 //.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-
                 // allow anonymous resource requests
                 .antMatchers(
                         HttpMethod.GET,
@@ -92,9 +88,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 ).permitAll().antMatchers("/api/**").permitAll()
                 .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated();
-        
-		httpSecurity.cors().and().authorizeRequests().anyRequest().authenticated();
-
         // Custom JWT based security filter
         httpSecurity
                 .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
