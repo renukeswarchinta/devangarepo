2bd4c2483061d1b6b4848e2e1ca50fff4eadf81f
Renukeswar <crenukeswar2010@gmail.com>
2017-01-22 10:06:38 +0530

    Changed native query

--- src/main/java/com/devangam/repository/EducationHelpingHandRepository.java
+++ src/main/java/com/devangam/repository/EducationHelpingHandRepository.java
@@ -10,8 +10,7 @@ import com.devangam.entity.Education;
 
 
 public interface EducationHelpingHandRepository extends JpaRepository<Education, Serializable>{
-public static final String FIND_ACTIVE_EDUCATION_DETAILS = "SELECT id,NAME,address,description,phone_number,current_education, family_annual_income,family_occupation,recent_merit_scored FROM T_EDUCATION";
 	
-	@Query(value="SELECT id,NAME,is_enable,address,description,last_update,phone_number,current_education, family_annual_income,family_occupation,recent_merit_scored FROM T_EDUCATION",nativeQuery=true)
+	@Query(value="SELECT id,NAME,is_enable,address,description,last_update,phone_number,current_education, family_annual_income,family_occupation,recent_merit_scored FROM t_education",nativeQuery=true)
 	public List<Education> findActiveEducationDetails();
 }
